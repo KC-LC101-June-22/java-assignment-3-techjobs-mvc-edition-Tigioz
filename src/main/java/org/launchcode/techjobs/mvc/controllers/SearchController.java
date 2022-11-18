7c12e28afe5bf2f3309f5051c2612e83d9320bc9
Tigioz <z.fouts@icloud.com>
2022-11-18 11:53:33 -0600

    added persistent radio selection, same test failing

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -38,6 +38,7 @@ public class SearchController {
         } else {
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
         }
+            model.addAttribute("searchType", searchType);
             model.addAttribute("columns", columnChoices);
             model.addAttribute("jobs", jobs);
         return "search";
